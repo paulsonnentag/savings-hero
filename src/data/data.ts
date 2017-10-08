691b38cecdd0cf05602dd8ad5fde4d6ba42e6fdc
timgrossmann <t.grossmann1994@web.de>
2017-10-08 06:06:25 -0700

    :star2: feat: categories with colors / bar plot started

---
+++ src/data/data.ts
@@ -0,0 +1,18 @@
+export interface Frequency {
+  letter: string,
+  frequency: number
+}
+
+export const StatsBarChart: Frequency[] = [
+  {letter: "A", frequency: .08167},
+  {letter: "B", frequency: .01492},
+  {letter: "C", frequency: .02782},
+  {letter: "D", frequency: .04253},
+  {letter: "E", frequency: .12702},
+  {letter: "F", frequency: .02288},
+  {letter: "G", frequency: .02015},
+  {letter: "H", frequency: .06094},
+  {letter: "I", frequency: .06966},
+  {letter: "J", frequency: .00153},
+  {letter: "K", frequency: .00772}
+];
